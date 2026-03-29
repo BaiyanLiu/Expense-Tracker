973f1e4ee124f23ac03538136ac6744869d2c468
Baiyan Liu <baiyanliu+github@protonmail.com>
2026-03-28 20:00:01 -0400

    Show all months for every year.

--- src/main/js/calendar.js
+++ src/main/js/calendar.js
@@ -34,29 +34,15 @@ export default class Calendar extends React.Component {
         return expensesByDate;
     }
 
-    getFirstYearAndMonth(expensesByDate) {
+    getYearsAndMonths(expensesByDate) {
         const years = Array.from(expensesByDate.keys());
         years.sort();
         const firstYear = years[0];
-
-        const months = Array.from(expensesByDate.get(firstYear)?.keys() ?? []);
-        months.sort();
-
-        return {firstYear, firstMonth: months[0]};
-    }
-
-    getYearsAndMonths(expensesByDate) {
-        const {firstYear, firstMonth} = this.getFirstYearAndMonth(expensesByDate);
-        const today = new Date();
-        const finalYear = today.getFullYear();
-        const finalMonth = today.getMonth();
+        const finalYear = new Date().getFullYear();
 
         const yearsAndMonths = [];
-
         for (let year = firstYear; year <= finalYear; year++) {
-            const firstMonthOfYear = year === firstYear ? firstMonth : 0;
-            const finalMonthOfYear = year === finalYear ? finalMonth : 11;
-            yearsAndMonths.push({year, months: Array.from({length: finalMonthOfYear - firstMonthOfYear + 1}, (_, i) => firstMonthOfYear + i)});
+            yearsAndMonths.push({year, months: Array.from({length: 12}, (_, i) => i)});
         }
 
         return yearsAndMonths;
