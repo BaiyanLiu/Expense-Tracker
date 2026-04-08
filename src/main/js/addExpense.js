c02ce2ec8c1a4755010f21350fed607cec92242d
Baiyan Liu <baiyanliu+github@protonmail.com>
2026-04-07 21:14:50 -0400

    Adding validation for add expense screen.

--- src/main/js/addExpense.js
+++ src/main/js/addExpense.js
@@ -1,16 +1,45 @@
 'use strict';
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function AddExpense({setIsAddingExpense}) {
+    const [type, setType] = useState("");
+    const [isTypeValid, setIsTypeValid] = useState(false);
+
+    const [amount, setAmount] = useState("");
+    const [isAmountValid, setIsAmountValid] = useState(false);
+
+    const [isSaveEnabled, setIsSaveEnabled] = useState(false);
+
+    useEffect(() => {
+        setIsTypeValid(type.length > 0);
+    }, [type])
+
+    useEffect(() => {
+        setIsAmountValid(/^\d+(\.\d{2})?$/.test(amount));
+    }, [amount])
+
+    useEffect(() => {
+        setIsSaveEnabled(isTypeValid && isAmountValid);
+    }, [isTypeValid, isAmountValid])
+
+    const onAmountChanged = (e) => {
+        setAmount(e.target.value.replace(/[^0-9.]/, ""));
+    }
+
+    const onSave = () => {
+        if (isSaveEnabled) {
+            setIsAddingExpense(false);
+        }
+    }
 
     return (
         <div className="add-expense">
-            <input type="text" placeholder="Type"/>
-            <input type="number" placeholder="0.00"/>
+            <input type="text" placeholder="Type" className={isTypeValid ? "" : "invalid-input"} value={type} onChange={(e) => setType(e.target.value)}/>
+            <input type="text" placeholder="Amount" className={isAmountValid ? "" : "invalid-input"} value={amount} onChange={onAmountChanged}/>
             <div className="add-expense-footer">
                 <div className="add-expense-cancel" onClick={() => setIsAddingExpense(false)}>Cancel</div>
-                <div className="add-expense-save" onClick={() => setIsAddingExpense(false)}>Save</div>
+                <div className={`add-expense-${isSaveEnabled ? "save" : "disabled"}`} onClick={onSave}>Save</div>
             </div>
         </div>
     );
